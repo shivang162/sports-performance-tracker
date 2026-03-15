c3f88653a2c3bbab0e6e7f7fa6cd9c2156834a44
shivang162 <kukretishivang@gmail.com>
2026-03-15 13:04:00 +0530

    Add PerformanceService with score calculation method

--- src/com/tracker/service/PerformanceService.java
+++ src/com/tracker/service/PerformanceService.java
@@ -1,1 +1,11 @@
+package com.tracker.service;
 
+public class PerformanceService {
+
+    public double calculateScore(double speed,double accuracy,double stamina){
+
+        double score = (speed * 0.4) + (accuracy * 0.3) + (stamina * 0.3);
+
+        return score;
+    }
+}
