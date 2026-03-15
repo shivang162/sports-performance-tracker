8f83432664b07d97753dad0dc2c6052af8c441c8
shivang162 <kukretishivang@gmail.com>
2026-03-15 12:55:35 +0530

    Update PerformanceLevel.java

--- src/com/tracker/service/PerformanceLevel.java
+++ src/com/tracker/service/PerformanceLevel.java
@@ -1,1 +1,19 @@
+package com.tracker.service;
 
+public class PerformanceLevel {
+
+    public String getLevel(double score){
+
+        if(score >= 85)
+            return "Excellent";
+
+        else if(score >= 70)
+            return "Good";
+
+        else if(score >= 50)
+            return "Average";
+
+        else
+            return "Needs Improvement";
+    }
+}
