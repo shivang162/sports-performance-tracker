1e9b19770d0a4a5f5bb22230ee28ed9474ed4e2b
SHIVANG KUKRETI <kukretishivang@gmail.com>
2026-03-23 19:09:45 +0530

    Implement main server functionality with HTTP server

---
+++ src/com/tracker/Main.java
@@ -0,0 +1,23 @@
+package com.tracker;
+
+import com.sun.net.httpserver.HttpServer;
+import com.tracker.controller.AuthController;
+import com.tracker.controller.PerformanceController;
+
+import java.net.InetSocketAddress;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/login", new AuthController());
+        server.createContext("/save", new PerformanceController());
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server running on http://localhost:8080");
+    }
+}
