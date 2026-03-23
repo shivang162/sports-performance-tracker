98def242101a85b5bb047bd575846064ac44f1a0
SHIVANG KUKRETI <kukretishivang@gmail.com>
2026-03-23 19:17:12 +0530

    Implement AuthController for handling login responses

--- src/com/tracker/Controller/AuthController.java
+++ src/com/tracker/Controller/AuthController.java
@@ -1,1 +1,25 @@
+package com.tracker.controller;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.OutputStream;
+
+public class AuthController implements HttpHandler {
+
+    public void handle(HttpExchange exchange) {
+
+        try {
+
+            String response = "Login Successful";
+
+            exchange.sendResponseHeaders(200, response.length());
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
