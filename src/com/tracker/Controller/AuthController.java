1f33235481f3a0e880c7bd2b22b743fe6c9c54a8
SHIVANG KUKRETI <kukretishivang@gmail.com>
2026-03-26 18:32:19 +0530

    Add newline at end of AuthController.java
    Fix formatting by adding a newline at the end of the file.

--- src/com/tracker/Controller/AuthController.java
+++ src/com/tracker/Controller/AuthController.java
@@ -3,7 +3,6 @@ package com.tracker.controller;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.tracker.service.AuthService;
-
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
