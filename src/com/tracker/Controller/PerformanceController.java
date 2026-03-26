bbcb34020c1ece087357779f90cd8d3c17a1e70c
SHIVANG KUKRETI <kukretishivang@gmail.com>
2026-03-26 18:32:53 +0530

    Refactor PerformanceController.java formatting
    Removed unnecessary blank line and added a newline at the end of the file.

--- src/com/tracker/Controller/PerformanceController.java
+++ src/com/tracker/Controller/PerformanceController.java
@@ -2,7 +2,6 @@ package com.tracker.controller;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
