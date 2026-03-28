9bdaf785464c10f6476e1ec1a4ad1b99a47c147a
SHIVANG KUKRETI <kukretishivang@gmail.com>
2026-03-28 10:15:12 +0530

    Update TestDB.java

--- TestDB.java
+++ TestDB.java
@@ -1,1 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class TestDB {
+
+    public static void main(String[] args) {
+
+        try {
+            Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/sports_tracker",
+                "root",
+                "SHIVANG123@k"
+            );
+
+            System.out.println("Connected to Database ✅");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
